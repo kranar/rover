f1dbb4c18ffbaa0d2f25a8c31cb2cfd09d5806a9
Kamal Mansouri <kamal@eidolonsystems.com>
2024-08-21 23:05:34 -0400

    Implemented the SupportVectorMachine with unit tests.

--- Include/Rover/SupportVectorMachine.hpp
+++ Include/Rover/SupportVectorMachine.hpp
@@ -1,6 +1,7 @@
 #ifndef ROVER_SUPPORT_VECTOR_MACHINE_HPP
 #define ROVER_SUPPORT_VECTOR_MACHINE_HPP
 #include <Eigen/Dense>
+#include "Rover/Optimizers.hpp"
 
 namespace Rover {
 
@@ -8,7 +9,7 @@ namespace Rover {
    * Implements a support vector machine to perform binary classification.
    * @param <S> The type of scalar to use.
    */
-  template<typename Scalar>
+  template<typename S>
   class SupportVectorMachine {
     public:
 
@@ -33,6 +34,52 @@ namespace Rover {
     private:
       Vector m_parameters;
   };
+
+  /**
+   * Trains a SupportVectorMachine.
+   * @param sample The sample to train the model with.
+   * @return A support vector machine trained on the <i>sample</i>.
+   */
+  template<typename Scalar>
+  SupportVectorMachine<Scalar> train_support_vector_machine(
+      const Eigen::MatrixX<Scalar>& sample) {
+    auto points = sample.leftCols(sample.cols() - 1);
+    auto targets = sample.rightCols(1);
+    auto parameters =
+      Eigen::VectorX<Scalar>(Eigen::VectorX<Scalar>::Zero(points.cols() + 1));
+    auto learning_rate = Scalar(0.01);
+    auto max_iterations = 1000;
+    for(int i = 0; i < max_iterations; ++i) {
+      for(int j = 0; j < points.rows(); ++j) {
+        auto x_i = points.row(j).transpose();
+        auto y_i = targets(j, 0);
+        auto extended_point = Eigen::VectorX<Scalar>(x_i.size() + 1);
+        extended_point << 1, x_i;
+        if(y_i * (parameters.dot(extended_point)) < 1) {
+          parameters += learning_rate * y_i * extended_point;
+        }
+      }
+      parameters.tail(points.cols()) -=
+        learning_rate * parameters.tail(points.cols());
+    }
+    return SupportVectorMachine(std::move(parameters));
+  }
+
+  template<typename Scalar>
+  SupportVectorMachine<Scalar>::SupportVectorMachine(Vector parameters)
+    : m_parameters(std::move(parameters)) {}
+
+  template<typename Scalar>
+  const typename SupportVectorMachine<Scalar>::Vector&
+      SupportVectorMachine<Scalar>::get_parameters() const {
+    return m_parameters;
+  }
+
+  template<typename Scalar>
+  bool SupportVectorMachine<Scalar>::evaluate(const Vector& point) const {
+    return m_parameters(0) +
+      m_parameters.tail(m_parameters.size() - 1).dot(point) >= 0;
+  }
 }
 
 #endif
---
+++ Source/Tests/SupportVectorMachineTester.cpp
@@ -0,0 +1,37 @@
+#include <doctest/doctest.h>
+#include "Rover/SupportVectorMachine.hpp"
+
+using namespace Eigen;
+using namespace Rover;
+
+TEST_SUITE("SupportVectorMachine") {
+  TEST_CASE("construction") {
+  }
+
+  TEST_CASE("evaluate") {
+  }
+
+  TEST_CASE("zero") {
+  }
+
+  TEST_CASE("train") {
+    auto sample = Eigen::MatrixXd(4, 3);
+    sample << 1.0, 2.0, 1.0,
+              2.0, 3.0, 1.0,
+              -1.0, -2.0, -1.0,
+              -2.0, -3.0, -1.0;
+    auto model = train_support_vector_machine(sample);
+    auto p1 = Eigen::VectorXd(2);
+    p1 << 1.0, 2.0;
+    REQUIRE(model.evaluate(p1));
+    auto p2 = Eigen::VectorXd(2);
+    p2 << 2.0, 3.0;
+    REQUIRE(model.evaluate(p2));
+    auto p3 = Eigen::VectorXd(2);
+    p3 << -1.0, -2.0;
+    REQUIRE(!model.evaluate(p3));
+    auto p4 = Eigen::VectorXd(2);
+    p4 << -2.0, -3.0;
+    REQUIRE(!model.evaluate(p4));
+  }
+}
